258e4eefcd377c2afbd568bd4e7c34bc567e10b0
OakJohn <changcheng5213@gmail.com>
2026-05-21 13:09:00 +0900

    test1
    from netlify

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE = 'diet-tracker-v3';
+const ASSETS = ['/', '/index.html', '/chart.min.js', '/manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if(e.request.mode === 'navigate'){
+    e.respondWith(
+      fetch(e.request).then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(cache => cache.put(e.request, clone));
+        return res;
+      }).catch(() => caches.match('/index.html'))
+    );
+    return;
+  }
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).then(res => {
+      if(res && res.status === 200 && e.request.method === 'GET'){
+        const clone = res.clone();
+        caches.open(CACHE).then(cache => cache.put(e.request, clone));
+      }
+      return res;
+    }).catch(() => caches.match('/index.html')))
+  );
+});
